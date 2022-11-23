09c4d5921f149239d184e669211eeed06b04f5a9
Lucas <lucasreis64@gmail.com>
2022-11-23 19:11:07 -0300

    chore: deploy backend

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRouters from "./routers/authRouters.js";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.use("/auth", authRouters);
+
+app.listen(process.env.PORT, () => {
+    console.log("Running on port " + process.env.PORT);
+});
