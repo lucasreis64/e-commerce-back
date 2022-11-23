080f4b3804514efa7369f8aa776f309a6518cac5
Antonio Prado <antonioeprado18@gmail.com>
2022-11-23 20:39:07 -0300

    feat: model function to create product in database

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { ObjectId } from "mongodb";
 import { products } from "../database/mongoDB";
 
@@ -14,7 +15,7 @@ export const Products = {
 			return false;
 		}
 	},
-	filterProductsByCategory: async function (obj) {
+	findProductsByCategory: async function (obj) {
 		try {
 			return await products.findOne({ category: obj.category });
 		} catch (error) {
@@ -25,9 +26,63 @@ export const Products = {
 	},
 	findAllProducts: async function (obj) {
 		try {
-			return await products.findOne({});
+			return await products.findMany({});
 		} catch (error) {
 			console.log(`Error trying to find all products in database.`);
+			console.log(`Operation returned: ${error}`);
+			return false;
+		}
+	},
+	createProduct: async function (obj) {
+		try {
+			const query = await this.findProductsByCategory(obj);
+			if (!query) {
+				return await products.insertOne({
+					category: obj.category,
+					products: [
+						{
+							_id: new ObjectId(),
+							title: obj.title,
+							img: obj.img,
+							description: obj.description,
+							price: obj.price,
+							views: 0,
+							inStock: obj.inStock,
+							dateAdded: dayjs().format("DD/MM/YYYY"),
+						},
+					],
+				});
+			}
+			return this.pushProduct(obj);
+		} catch (error) {
+			console.log(`Error trying to create ${obj.title} in database.`);
+			console.log(`Operation returned: ${error}`);
+			return false;
+		}
+	},
+	pushProduct: async function (obj) {
+		try {
+			const _id = new ObjectId();
+			const query = await products.updateOne(
+				{ category: obj.category },
+				{
+					$push: {
+						products: {
+							_id,
+							title: obj.title,
+							img: obj.img,
+							description: obj.description,
+							price: obj.price,
+							views: 0,
+							inStock: obj.inStock,
+							dateAdded: dayjs().format("DD/MM/YYYY"),
+						},
+					},
+				}
+			);
+			return { _id, query };
+		} catch (error) {
+			console.log(`Error trying to create ${obj.title} in database.`);
 			console.log(`Operation returned: ${error}`);
 			return false;
 		}
