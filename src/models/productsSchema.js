7d68442c4b30d2f5f0f6f9ea384ad9f0eb1e886a
Lucas <lucasreis64@gmail.com>
2022-11-25 16:55:51 -0300

    feat: img by url, change max length

--- src/models/productsSchema.js
+++ src/models/productsSchema.js
@@ -2,11 +2,11 @@ import Joi from "joi";
 
 export const productsSchema = Joi.object({
 	category: Joi.string().required(),
-	title: Joi.string().max(20).required(),
+	title: Joi.string().max(40).required(),
 	img: Joi.string()
-		.pattern(new RegExp(/\/products\/[\w]+.png/))
+		.uri()
 		.required(),
-	description: Joi.string().max(60).min(20).required(),
+	description: Joi.string().max(120).min(20).required(),
 	price: Joi.number().required(),
 	inStock: Joi.number().min(1).required(),
 });
