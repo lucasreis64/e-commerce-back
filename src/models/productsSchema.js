b3353d2ba6f36a3056909fdf52af39a04fc8cdf0
Antonio Prado <antonioeprado18@gmail.com>
2022-11-27 17:09:57 -0300

    fix: changed max character on product description schema

--- src/models/productsSchema.js
+++ src/models/productsSchema.js
@@ -4,11 +4,7 @@ export const productsSchema = Joi.object({
 	category: Joi.string().required(),
 	title: Joi.string().max(40).required(),
 	img: Joi.string().uri().required(),
-<<<<<<< HEAD
 	description: Joi.string().max(120).min(20).required(),
-=======
-	description: Joi.string().max(100).min(20).required(),
->>>>>>> 179d98f (fix: added more characters to description product schema)
 	price: Joi.number().required(),
 	inStock: Joi.number().min(1).required(),
 });
