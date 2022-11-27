630e0f18a07df66523fbbbba6d547568e85139fe
Antonio Prado <antonioeprado18@gmail.com>
2022-11-27 10:24:25 -0300

    refactor: tweaked img model path

--- src/models/productsSchema.js
+++ src/models/productsSchema.js
@@ -3,14 +3,8 @@ import Joi from "joi";
 export const productsSchema = Joi.object({
 	category: Joi.string().required(),
 	title: Joi.string().max(40).required(),
-	img: Joi.string()
-<<<<<<< HEAD
-		.pattern(new RegExp(/products\/[\w]+.png/))
-=======
-		.uri()
->>>>>>> 7d68442 (feat: img by url, change max length)
-		.required(),
-	description: Joi.string().max(60).min(20).required(),
+	img: Joi.string().uri().required(),
+	description: Joi.string().max(120).min(20).required(),
 	price: Joi.number().required(),
 	inStock: Joi.number().min(1).required(),
 });
