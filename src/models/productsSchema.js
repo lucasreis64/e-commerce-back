c1f4375f5b69f9656485e770d42ea3314bfa2ce3
Antonio Prado <antonioeprado18@gmail.com>
2022-11-27 10:24:25 -0300

    refactor: tweaked img model path

--- src/models/productsSchema.js
+++ src/models/productsSchema.js
@@ -4,7 +4,7 @@ export const productsSchema = Joi.object({
 	category: Joi.string().required(),
 	title: Joi.string().max(20).required(),
 	img: Joi.string()
-		.pattern(new RegExp(/\/products\/[\w]+.png/))
+		.pattern(new RegExp(/products\/[\w]+.png/))
 		.required(),
 	description: Joi.string().max(60).min(20).required(),
 	price: Joi.number().required(),
